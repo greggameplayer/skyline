cce2c0af4bfe5d354de31ac5b00f428e0a325927
Billy Laws <blaws05@gmail.com>
2020-07-05 18:52:30 +0100

    DNM: hacky pre-patching implementation
    This should be done properly sometime, we need to calculate the size
    rather than hardcoding it. We also need to put a trampoline at the start
    to clear registers.

--- app/src/main/cpp/skyline/loader/loader.cpp
+++ app/src/main/cpp/skyline/loader/loader.cpp
@@ -8,7 +8,10 @@
 
 namespace skyline::loader {
     Loader::ExecutableLoadInfo Loader::LoadExecutable(const std::shared_ptr<kernel::type::KProcess> process, const DeviceState &state, Executable &executable, size_t offset) {
-        u64 base = constant::BaseAddress + offset;
+        u64 patchRegionSize = PAGE_SIZE * 0x10;
+        u64 base = constant::BaseAddress + offset - patchRegionSize;
+
+        u64 executableBase = base + patchRegionSize;
 
         u64 textSize = executable.text.contents.size();
         u64 roSize = executable.ro.contents.size();
@@ -21,29 +24,26 @@ namespace skyline::loader {
             throw exception("LoadProcessData: Section offsets are not aligned with page size: 0x{:X}, 0x{:X}, 0x{:X}", executable.text.offset, executable.ro.offset, executable.data.offset);
 
         // The data section will always be the last section in memory, so put the patch section after it
-        u64 patchOffset = executable.data.offset + dataSize;
-        std::vector<u32> patch = state.nce->PatchCode(executable.text.contents, base, patchOffset);
-
+        std::vector<u32> patch = state.nce->PatchCode(executable.text.contents, executableBase, -patchRegionSize);
         u64 patchSize = patch.size() * sizeof(u32);
-        u64 padding = util::AlignUp(patchSize, PAGE_SIZE) - patchSize;
+        
+        process->NewHandle<kernel::type::KPrivateMemory>(base, patchRegionSize, memory::Permission{true, true, true}, memory::states::CodeMutable); // RWX
+        state.logger->Debug("Successfully mapped section .patch @ 0x{0:X}, Size = 0x{1:X}", base, patchRegionSize);
 
-        process->NewHandle<kernel::type::KPrivateMemory>(base + executable.text.offset, textSize, memory::Permission{true, false, true}, memory::states::CodeStatic); // R-X
-        state.logger->Debug("Successfully mapped section .text @ 0x{0:X}, Size = 0x{1:X}", base + executable.text.offset, textSize);
+        process->NewHandle<kernel::type::KPrivateMemory>(executableBase + executable.text.offset, textSize, memory::Permission{true, false, true}, memory::states::CodeStatic); // R-X
+        state.logger->Debug("Successfully mapped section .text @ 0x{0:X}, Size = 0x{1:X}", executableBase + executable.text.offset, textSize);
 
-        process->NewHandle<kernel::type::KPrivateMemory>(base + executable.ro.offset, roSize, memory::Permission{true, false, false}, memory::states::CodeReadOnly); // R--
-        state.logger->Debug("Successfully mapped section .rodata @ 0x{0:X}, Size = 0x{1:X}", base + executable.ro.offset, roSize);
+        process->NewHandle<kernel::type::KPrivateMemory>(executableBase + executable.ro.offset, roSize, memory::Permission{true, false, false}, memory::states::CodeReadOnly); // R--
+        state.logger->Debug("Successfully mapped section .rodata @ 0x{0:X}, Size = 0x{1:X}", executableBase + executable.ro.offset, roSize);
 
-        process->NewHandle<kernel::type::KPrivateMemory>(base + executable.data.offset, dataSize, memory::Permission{true, true, false}, memory::states::CodeMutable); // RW-
-        state.logger->Debug("Successfully mapped section .data @ 0x{0:X}, Size = 0x{1:X}", base + executable.data.offset, dataSize);
+        process->NewHandle<kernel::type::KPrivateMemory>(executableBase + executable.data.offset, dataSize, memory::Permission{true, true, false}, memory::states::CodeMutable); // RW-
+        state.logger->Debug("Successfully mapped section .data @ 0x{0:X}, Size = 0x{1:X}", executableBase + executable.data.offset, dataSize);
 
-        process->NewHandle<kernel::type::KPrivateMemory>(base + patchOffset, patchSize + padding, memory::Permission{true, true, true}, memory::states::CodeMutable); // RWX
-        state.logger->Debug("Successfully mapped section .patch @ 0x{0:X}, Size = 0x{1:X}", base + patchOffset, patchSize + padding);
+        process->WriteMemory(patch.data(), base, patchSize);
+        process->WriteMemory(executable.text.contents.data(), executableBase + executable.text.offset, textSize);
+        process->WriteMemory(executable.ro.contents.data(), executableBase + executable.ro.offset, roSize);
+        process->WriteMemory(executable.data.contents.data(), executableBase + executable.data.offset, dataSize - executable.bssSize);
 
-        process->WriteMemory(executable.text.contents.data(), base + executable.text.offset, textSize);
-        process->WriteMemory(executable.ro.contents.data(), base + executable.ro.offset, roSize);
-        process->WriteMemory(executable.data.contents.data(), base + executable.data.offset, dataSize - executable.bssSize);
-        process->WriteMemory(patch.data(), base + patchOffset, patchSize);
-
-        return {base, patchOffset + patchSize + padding};
+        return {base ,  patchRegionSize + executable.data.offset + dataSize};
     }
 }
